482ac8822f64a2863681b899d8f0a75d93079c0b
marcus delbianco <mdelbianco9@gmail.com>
2018-02-17 22:33:00 -0800

    Set up and installed react project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+// Nest All components into this component!
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, I am React App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
