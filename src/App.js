82051c343e60b63c2df772672f09455c37c100ad
marcus delbianco <mdelbianco9@gmail.com>
2018-03-08 21:30:38 -0800

    Added styling and 2way binding

--- src/App.js
+++ src/App.js
@@ -22,10 +22,30 @@ class App extends Component {
     })
   }
 
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 26},
+        { name: event.target.value, age: 19},
+        { name: 'Mark', age: 40}
+    ]
+    })
+  }
+
   render() {
+    const style = {
+        backgroundColor: 'white',
+        font: 'inherit',
+        border: '1px solid blue',
+        padding: '8px',
+        margin: '20px',
+        cursor: 'pointer'
+      };
+
     return (
       <div className="App">
-        <button onClick={this.switchNameHandler.bind(this, 'Maxamillion')}>Switch Name</button>
+        <button style={style}
+        onClick={this.switchNameHandler.bind(this, 'Maxamillion')}>Switch Name</button>
 
         <Person 
         name={this.state.persons[0].name} 
@@ -38,7 +58,8 @@ class App extends Component {
         <Person 
         name={this.state.persons[2].name} 
         age={this.state.persons[2].age}
-        click={this.switchNameHandler.bind(this, 'Max!')}>My Hobbies: Racing</Person>
+        click={this.switchNameHandler.bind(this, 'Max!')}
+        changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
 
         
       </div>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,13 +1,15 @@
 // A component is just a function that returns JSX
 
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
 	// WHen using the {} you can only use simple one line expressions, nothing too crazy
 	return (
-		<div>
+		<div className="Person">
 			<p onClick={props.click}>I am a {props.name} and Im {props.age} years old</p>
 			<p>{props.children}</p>
+			<input type="text" onChange={props.changed}/>
 		</div>
 		)
 };
