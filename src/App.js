a01b6e0e7f565073f005c14e08378142aa334398
marcus delbianco <mdelbianco9@gmail.com>
2018-03-10 23:25:51 -0800

    Lists

--- src/App.js
+++ src/App.js
@@ -9,27 +9,33 @@ class App extends Component {
       { name: 'Marcus', age: 26},
       { name: 'John', age: 19},
       { name: 'Mark', age: 30}
-    ]
-  }
-
-  switchNameHandler = (newName) => {
-    this.setState({
-      persons: [
-        { name: newName, age: 26},
-        { name: 'John', age: 19},
-        { name: 'Mark', age: 40}
-    ]
-    })
+    ],
+    otherState: 'some value',
+    showPersons: false
   }
 
   nameChangedHandler = (event) => {
     this.setState({
       persons: [
         { name: 'Max', age: 26},
-        { name: event.target.value, age: 19},
-        { name: 'Mark', age: 40}
+        { name: event.target.value, age: 36},
+        { name: event.target.value, age: 20}
     ]
     })
+  }
+
+  deletePersonHandler = (personIndex) => {
+    // The line below creates a new array (copies it) instead of using the one in the state.
+    // const persons = this.state.persons.slice();
+    // Or use the spread operator
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
   }
 
   render() {
@@ -42,26 +48,38 @@ class App extends Component {
         cursor: 'pointer'
       };
 
+      let persons = null;
+
+     /*Just use pure Vanilla JS to say "if the showPersons value is true then 
+      Create the elements in JSX*/
+
+      // You can use map() to add a function (think of an action) to create the JSX Person components
+/* The code below adds the name and age values from the persons array in state and returns
+the Person component. The person in persons is like what you use in Vue. The person in the 
+map() function can be anything, its just an arbatrary name variable given to the 
+value sinside the persons arry in state*/ 
+
+      if (this.state.showPersons) {
+        persons = (
+          <div>
+            {this.state.persons.map((person, index) => {
+              return <Person 
+                      click={() => this.deletePersonHandler(index)}
+                      name={person.name} 
+                      age={person.age} />
+            })}
+          </div> 
+          );
+      }
+
     return (
       <div className="App">
+
         <button style={style}
-        onClick={this.switchNameHandler.bind(this, 'Maxamillion')}>Switch Name</button>
+        onClick={this.togglePersonsHandler}>Toggle Persons</button>
 
-        <Person 
-        name={this.state.persons[0].name} 
-        age={this.state.persons[0].age} />
+        {persons}
 
-        <Person 
-        name={this.state.persons[1].name} 
-        age={this.state.persons[1].age} />
-
-        <Person 
-        name={this.state.persons[2].name} 
-        age={this.state.persons[2].age}
-        click={this.switchNameHandler.bind(this, 'Max!')}
-        changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
-
-        
       </div>
     );
 
